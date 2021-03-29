c25a95086dce9e383930579fb709e83582409e9d
Short Stack <41342213+rchlblns@users.noreply.github.com>
2021-03-29 01:30:44 -0500

    changed visual feedback for click, include visual feedback for opening dropdown menu

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
 
-    $("#subnav-trigger").hover(function() {
+    $("#subnav-trigger").click(function() {
         $(".subnav-content").slideToggle("slow");
+        $(".material-icons").toggleClass("down");
+        $("li a").removeClass("active");
+        $(this).addClass("active-dropdown")
     });
+
+    $("#subnav-trigger ul").click(function(e) {
+        e.stopPropagation();
+    })
 
     $("ul li a").click(function() {
         $("li a").removeClass("active");
         $(this).addClass("active")
     })
+
 })
